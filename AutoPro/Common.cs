006d03934183ae7fff54bebe7e5f6ada19325e7b
Pham Van Quyet <u23702@vn.umc>
2018-09-05 13:34:02 +0700

    auto for netframework 2.0

---
+++ AutoPro/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutoPro
+{
+    public class Common
+    {
+        public static void SaveData(string path, string data)
+        {
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(data);
+            }
+        }
+    }
+}
